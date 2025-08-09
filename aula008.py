04ee87a544cb33f530fcbba5a828014060dd4890
IgorEustachio <igor.daniel.eustachio@gmail.com>
2025-08-09 20:07:59 -0300

    + aula 008

---
+++ aula008.py
@@ -0,0 +1,6 @@
+import math
+from math import sqrt, ceil
+
+n=int(input('digite um numero: '))
+raiz=math.sqrt(n)
+print(f'a raiz de {n} é igual a: {math.ceil(raiz):.2f}')
---
+++ aula008b.py
@@ -0,0 +1,2 @@
+import emoji
+print(emoji.emojize('oi :globe_showing_americas:', language='alias'))
